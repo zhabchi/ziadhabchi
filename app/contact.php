889dddc455e9e6153e6134332b9af473f42797a7
zhabchi <ziad.habchi@gmail.com>
2014-10-31 16:28:34 +0200

    config

--- app/contact.php
+++ app/contact.php
@@ -1,12 +1,5 @@
 <?php
-/*
- * A Design by W3layouts
-Author: W3layouts
-Author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
- *
- */			
+			
  			$userName=$_REQUEST['userName'];
 			$userEmail=$_REQUEST['userEmail'];
 			$userPhone=$_REQUEST['userPhone'];
