7611ecd982b6311ae54ed6618fd46c50ec5de1ef
zhabchi <ziad.habchi@gmail.com>
2014-10-31 16:18:02 +0200

    first changes

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php 
-/*
- * A Design by W3layouts
-Author: W3layouts
-Author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
- *
- */
+
 include "app/config.php";
 include "app/detect.php";
 
